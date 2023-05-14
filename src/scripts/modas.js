c13e513b70c96a90c26e8b713661ec95dc610a22
Nata Spitz Alves <nataspitz81@gmail.com>
2023-05-14 17:20:00 -0300

    fix: corrige bug de fechar modais

--- src/scripts/modas.js
+++ src/scripts/modas.js
@@ -60,6 +60,7 @@ export async function viewDepartment(department) {
     const company = sector.company
     const modal = document.querySelector(".modal__viewDepartment")
     const h2 = document.querySelector(".modal__viewDepartment > h2")
+    const buttonX = document.querySelector(".viewDepartment__exit")
     const pDescription = document.querySelector(".modal__description")
     const select = document.querySelector(".select__noDepartment")
     const button = document.querySelector("#hire")
@@ -110,6 +111,10 @@ export async function viewDepartment(department) {
             modal.showModal()
         })
     })
+
+    buttonX.addEventListener("click", () =>{
+        modal.close()
+})
     
     
 }
@@ -117,31 +122,30 @@ export async function viewDepartment(department) {
 export function editDeptartment(department) {
     const modal = document.querySelector(".modal__editDepartment")
     const button = document.querySelector("#editDepartment")
-    const buttonX = document.querySelector(".button__X")
+    const buttonX = document.querySelector(".editDepartment__exit")
     const textarea = document.querySelector("form > textarea")
     textarea.innerHTML = department.description
-    console.log(department);
     let dataBase = {}
+    
+    buttonX.addEventListener("click", () =>{
+       modal.close()
+    })
 
     button.addEventListener("click",async  (event) =>{
         event.preventDefault()
         dataBase[textarea.name] = textarea.value
         await requestEdiitDepartment(department.id, dataBase)
-        console.log(department.id,dataBase);
-        modal.xlose()
-
-    })
-
-    buttonX.addEventListener("click", () =>{
         modal.close()
+        
     })
+    
 
     
 }
 
 export function deleteDepartment(department) {
     const modal = document.querySelector(".modal__deleteDepartment")
-    const buttonX = document.querySelector(".button__X")
+    const buttonX = document.querySelector(".deleteDepartment__exit")
     const span = document.querySelector(".span__name")
     const button = document.querySelector("#removeDepartment")
     span.innerText = department.name
@@ -158,39 +162,40 @@ export function deleteDepartment(department) {
     })
     
     buttonX.addEventListener("click", () =>{
-        const modal = document.querySelector(".modal__deleteDepartment")
         modal.close()
     })
     
 }
 
 export function editUser(user) {
+    const modal = document.querySelector(".modal__editUser")
+    const buttonX = document.querySelector(".editUser__exit")
     const  inputs = document.querySelectorAll(".editUser__form  input")
     const button  = document.querySelector("#editUser")
     let dataBase = {}
 
-    
+  button.addEventListener("click", (event) =>{
+    event.preventDefault()
 
-    inputs.forEach( input => {
-    
-      console.log(dataBase[input.name] = input.innerText)
-        
+    inputs.forEach(input =>{
+        if (input.value.length) {
+            dataBase[input.name] = input.value
+        }
     })
+
+    requestEditUser(user.id, dataBase)
+    modal.close()
+    renderAllEmployees()
+  })
     
-    console.log(dataBase);
-    button.addEventListener("click", async (event) =>{
-        event.preventDefault()
-        const modal = document.querySelector(".modal__editUser")
-        console.log(dataBase);
-         await requestEditUser(user.id, dataBase)
-        modal.close()
-    })
-    
+  buttonX.addEventListener("click", () =>[
+    modal.close()
+  ])
 }
 
 export function deleteUser(user) {
     const modal = document.querySelector(".modal__deleteUser")
-    const buttonX = document.querySelector(".button__X")
+    const buttonX = document.querySelector(".deleteUser__exit")
     const span = document.querySelector("#text__usuario")
     const button = document.querySelector(".removeUser")
     span.innerHTML = user.name
